dec4f14ce2725577045406eaabf9fcd8de47b131
Sven Wappler <wappler@wappler.systems>
2025-02-05 23:28:16 +0100

    init

---
+++ Classes/Powermail/EventListener/FormControllerCreateActionBeforeRenderViewEventListener.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WapplerSystems\MailEnd2End\Powermail\EventListener;
+
+final class FormControllerCreateActionBeforeRenderViewEventListener
+{
+
+}
---
+++ ext_emconf.php
@@ -0,0 +1,17 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Email end to end testing',
+    'description' => '',
+    'category' => 'fe',
+    'version' => '12.0.0',
+    'state' => 'stable',
+    'author' => 'Sven Wappler',
+    'author_email' => 'typo3YYYY@wappler.systems',
+    'author_company' => 'WapplerSystems',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+    ],
+];
