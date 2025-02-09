4b906dd005ff1f59bf7ff4023eab42e35bfffc50
Sven Wappler <wappler@wappler.systems>
2025-02-09 12:53:43 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Formular end to end testing',
     'description' => 'Formular end 2 end tests for TYPO3 form and powermail. Changes the email addresses by request header.',
     'category' => 'fe',
-    'version' => '12.0.0',
+    'version' => '13.0.0',
     'state' => 'stable',
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.4.99',
-            'form_extended' => '12.0.0',
+            'typo3' => '13.0.0-13.4.99',
+            'form_extended' => '13.0.0',
         ],
     ],
 ];
