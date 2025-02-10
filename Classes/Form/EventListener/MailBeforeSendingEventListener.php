a68a67ff9e5239b639d01528c6b3b61006511b10
Sven Wappler <wappler@wappler.systems>
2025-02-10 19:25:57 +0100

    add remote host list

--- Classes/Form/EventListener/MailBeforeSendingEventListener.php
+++ Classes/Form/EventListener/MailBeforeSendingEventListener.php
@@ -26,7 +26,10 @@ final class MailBeforeSendingEventListener
         $key = GeneralUtility::makeInstance(ExtensionConfiguration::class)
             ->get('form_end2end', 'key');
 
-        if ($e2eKey !== '' && $e2eKey === $key) {
+        $remoteHosts = explode(',',GeneralUtility::makeInstance(ExtensionConfiguration::class)
+            ->get('form_end2end', 'remoteHosts'));
+
+        if (($e2eKey !== '' && $e2eKey === $key) || $this->checkHostAgainstPatterns($request->getServerParams()['REMOTE_ADDR'], $remoteHosts)) {
 
             $email = GeneralUtility::makeInstance(ExtensionConfiguration::class)
                 ->get('form_end2end', 'email');
@@ -44,6 +47,10 @@ final class MailBeforeSendingEventListener
             /** @var SiteLanguage $siteLanguage */
             $siteLanguage = $request->getAttribute('language');
 
+            $tos = $mail->getTo();
+            foreach ($tos as $to) {
+                $mail->getHeaders()->addTextHeader('X-E2E-Orig-To', $to->getAddress());
+            }
             $mail->getHeaders()->addTextHeader('X-E2E-Form', $event->getFinisherContext()->getFormRuntime()->getFormDefinition()->getPersistenceIdentifier());
             $mail->getHeaders()->addTextHeader('X-E2E-Formtype', $type);
             $mail->getHeaders()->addTextHeader('X-E2E-Language', $siteLanguage->getLocale()->getName());
@@ -54,5 +61,21 @@ final class MailBeforeSendingEventListener
         }
     }
 
+
+    private function checkHostAgainstPatterns($host, $patterns): bool
+    {
+        foreach ($patterns as $pattern) {
+            if ($pattern === '') {
+                continue; // Leeres Muster überspringen
+            }
+            // Ersetzen von Wildcards (*) durch reguläre Ausdrucks-Syntax (.*)
+            $regex = '/^' . str_replace('\*', '.*', preg_quote($pattern, '/')) . '$/';
+
+            if (preg_match($regex, $host)) {
+                return true; // Übereinstimmung gefunden
+            }
+        }
+        return false; // Keine Übereinstimmung gefunden
+    }
 
 }
